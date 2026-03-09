281f073163dee15bf61ff740e9b04127fc255e6f
y0dac <122667936+y0dac@users.noreply.github.com>
2026-03-09 20:00:03 +0200

    init commit: file structure + CI/CD

---
+++ my_app/lib/core/network/dio_client.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+
+class DioClient {
+  late final Dio dio;
+  
+  DioClient({required String baseUrl}) {
+    dio = Dio(BaseOptions(
+      baseUrl: baseUrl,
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
+      headers: {'Content-Type': 'application/json'},
+    ));
+    
+    dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestHeader: true,
+      requestBody: true,
+      responseHeader: true,
+      responseBody: true,
+    ));
+  }
+}
---
+++ my_app/test/widget_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Initial test passes', () {
+    expect(true, isTrue);
+  });
+}
