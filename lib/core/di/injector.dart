18a35bd5051360841592c989ac01e5da9008ffd2
y0dac <122667936+y0dac@users.noreply.github.com>
2026-03-10 23:41:22 +0200

    feat: add router and update app with DI

---
+++ lib/core/di/injector.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import '../network/dio_client.dart';
+
+final getIt = GetIt.instance;
+
+void setupDependencies() {
+  // Register core services
+  getIt.registerSingleton<DioClient>(
+    DioClient(baseUrl: const String.fromEnvironment('API_URL')),
+  );
+  
+  // Feature dependencies will be added here as we build them
+}
