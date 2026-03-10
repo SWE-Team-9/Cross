038012159515f091f7c60ade3697d5a977a8f06b
y0dac <122667936+y0dac@users.noreply.github.com>
2026-03-10 23:53:12 +0200

    style: apply dart format

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -8,6 +8,6 @@ void setupDependencies() {
   getIt.registerSingleton<DioClient>(
     DioClient(baseUrl: const String.fromEnvironment('API_URL')),
   );
-  
+
   // Feature dependencies will be added here as we build them
 }
