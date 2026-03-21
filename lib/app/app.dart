33e5f5f7bc8f60dd9262ad72f001562eb944fba5
Ahmed <mando2005a@gmail.com>
2026-03-21 03:05:25 +0200

    fix code formating

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -11,7 +11,6 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      
       create: (_) => getIt<AuthCubit>(),
       child: MaterialApp.router(
         title: 'SoundCloud Clone',
--- lib/features/social/data/repositories/social_repo.dart
+++ lib/features/social/data/repositories/social_repo.dart
@@ -1,20 +1,17 @@
 import 'package:soundcloud_clone/features/social/domain/entities/user.dart';
 
 class SocialRepo {
-
   final List<User> _users = [
     User(id: '1', username: 'Ahmed'),
     User(id: '2', username: 'Ali'),
     User(id: '3', username: 'Sara'),
   ];
 
-
   Future<List<User>> getFollowers(String userId, int page) async {
     await Future.delayed(const Duration(milliseconds: 500));
 
     return _users;
   }
-
 
   Future<List<User>> getFollowing(String userId, int page) async {
     await Future.delayed(const Duration(milliseconds: 500));
@@ -25,15 +22,14 @@ class SocialRepo {
   // 👤 FOLLOW
   Future<bool> followUser(String userId) async {
     // TODO: backend later
-     await Future.delayed(const Duration(milliseconds: 300));
+    await Future.delayed(const Duration(milliseconds: 300));
     return true;
-    
   }
 
   // 👤 UNFOLLOW
   Future<bool> unfollowUser(String userId) async {
     // TODO: backend later
-     await Future.delayed(const Duration(milliseconds: 300));
+    await Future.delayed(const Duration(milliseconds: 300));
     return true;
   }
 
