ab3e9b7e3daecbdd2c5607caf6d194821f411d1e
AliIbrahim174 <141499297+AliIbrahim174@users.noreply.github.com>
2026-03-25 11:07:14 +0200

    fix(dart format)

--- test/core/services/audio_player_service_test.dart
+++ test/core/services/audio_player_service_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:soundcloud_clone/core/models/player_state.dart';
@@ -10,7 +9,6 @@ void main() {
 
   late JustAudioPlayerService service;
   late RecentlyPlayedCubit recentlyPlayedCubit;
-
 
   setUp(() {
     GetIt.I.reset();
