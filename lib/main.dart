183eeb0f765cb88248c3628cfd0f99af861484f2
Ali-Mahmoud-Ali <alymaly24@gmail.com>
2026-04-11 21:33:40 +0200

    feat(playback): refactor audio service to use AudioHandler for background playback

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 import 'core/di/injector.dart';
+import 'package:audio_service/audio_service.dart';
+import 'core/audio/app_audio_handler.dart';
+
+late AudioHandler audioHandler;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  audioHandler = await AudioService.init(
+    builder: () => AppAudioHandler(),
+    config: AudioServiceConfig(
+      androidNotificationChannelId: 'com.soundcloud.app',
+      androidNotificationChannelName: 'Playback',
+      androidNotificationOngoing: true,
+      androidStopForegroundOnPause: true,
+    ),
+  );
+
   await setupDependencies();
+
   runApp(const App());
 }
