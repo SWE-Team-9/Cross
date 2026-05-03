cab66190391b7c7faedf2fb8382f9e6680360d31
AliIbrahim174 <141499297+AliIbrahim174@users.noreply.github.com>
2026-05-03 03:29:37 +0300

    test(premium): update billing return fake repository

--- test/features/premium/presentation/pages/billing_page_return_test.dart
+++ test/features/premium/presentation/pages/billing_page_return_test.dart
@@ -279,6 +279,7 @@ class _FakeSubscriptionRepository extends SubscriptionRepository {
   int cancelSubscriptionCalls = 0;
   int resumeSubscriptionCalls = 0;
   int changePlanCalls = 0;
+    int cancelPlanChangeCalls = 0;
   int getOfflineTrackEntitlementCalls = 0;
 
   String? lastCheckoutPlan;
@@ -369,7 +370,14 @@ class _FakeSubscriptionRepository extends SubscriptionRepository {
 
     return subscription;
   }
+  @override
+  Future<Subscription> cancelPlanChange() async {
+    cancelPlanChangeCalls++;
 
+    subscription = subscription.copyWith(clearPendingDowngrade: true);
+
+    return subscription;
+  }
   @override
   Future<OfflineTrackEntitlement> getOfflineTrackEntitlement(
     String trackId,
